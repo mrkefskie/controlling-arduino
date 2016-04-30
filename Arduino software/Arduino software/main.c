05a7e6b48b3eadcf9d2d258106dfb8ac77c7387e
mrkefskie <mrkefskie@gmail.com>
2016-04-30 17:01:08 +0200

    Added basic files for arduino software

---
+++ Arduino software/Arduino software/main.c
@@ -0,0 +1,18 @@
+/*
+ * Arduino software.c
+ *
+ * Created: 30-4-2016 17:00:40
+ * Author : Kevin Slagmolen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
